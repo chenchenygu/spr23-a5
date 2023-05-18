05d49c150354d521d77ee40c9fc6fbe2a5349103
Chenchen Gu <cygu@stanford.edu>
2023-05-18 00:05:50 -0700

    Add tests to your_tests.py

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,9 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
-
+assert run("20 / 4").output == "5"
+assert run("8 - 1").output == "7"
+assert run("25").exit_status != 0
 ###
 
 print("All tests passed!")
